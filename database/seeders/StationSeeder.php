9b7fcc923cf14cdfa8ed20d888bd97002aabb714
Hortplus-Millar <millar@hortplus.com>
2021-06-26 05:21:08 +0000

    Added station model

---
+++ database/seeders/StationSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('stations')->insert([
+            'name' => 'Test Station One',
+            'latitude' => -39.65485,
+            'longitude' => 176.87624
+        ]);
+
+        DB::table('stations')->insert([
+            'name' => 'Test Station One',
+            'latitude' => -39.87859,
+            'longitude' => 176.43221
+        ]);
+    }
+}
